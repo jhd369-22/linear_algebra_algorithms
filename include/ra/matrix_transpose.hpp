ed456e67d8b839c374e2fd53ceb5565335cf6c9a
Junhong Deng <dengjunhong@gmail.com>
2022-07-13 12:54:48 -0700

    fix: add an auxiliary array to handle the aliasing case that when pointer a and pointer b point to same array

--- include/ra/matrix_transpose.hpp
+++ include/ra/matrix_transpose.hpp
@@ -1,41 +1,42 @@
+#include <algorithm>  // std::copy
 #include <cstdio>
-#include <iostream>
 
 namespace ra::cache {
     // cache-oblivious matrix transpose
     template <typename T>
-    bool matrix_transpose_helper(const T* a, std::size_t m, std::size_t n, T* b,
+    void matrix_transpose_helper(const T* a, std::size_t m, std::size_t n, T* b,
                                  std::size_t t_m, std::size_t t_n) {
-        bool flag = false;
 
         if (t_m * t_n <= 4) {
             for (std::size_t i = 0; i < t_m; ++i) {
                 for (std::size_t j = 0; j < t_n; ++j) {
-                    if (b[j * m + i] == a[i * n + j]) {
-                        flag = true;
-                    }
-
                     b[j * m + i] = a[i * n + j];
                 }
             }
-
-            return flag;
         } else {
             if (t_m < t_n) {
-                flag |= matrix_transpose_helper(a, m, n, b, t_m, t_n / 2);
-                flag |= matrix_transpose_helper(a + (t_n / 2), m, n, b + (t_n / 2) * m, t_m, t_n - t_n / 2);
-                return flag;
+                matrix_transpose_helper(a, m, n, b, t_m, t_n / 2);
+                matrix_transpose_helper(a + (t_n / 2), m, n, b + (t_n / 2) * m, t_m, t_n - t_n / 2);
             } else {
-                flag |= matrix_transpose_helper(a, m, n, b, t_m / 2, t_n);
-                flag |= matrix_transpose_helper(a + (t_m / 2) * n, m, n, b + (t_m / 2), t_m - t_m / 2, t_n);
-                return flag;
+                matrix_transpose_helper(a, m, n, b, t_m / 2, t_n);
+                matrix_transpose_helper(a + (t_m / 2) * n, m, n, b + (t_m / 2), t_m - t_m / 2, t_n);
             }
         }
+
+        return;
     }
 
     template <class T>
     void matrix_transpose(const T* a, std::size_t m, std::size_t n, T* b) {
-        matrix_transpose_helper(a, m, n, b, m, n);
+
+        if(a == b) {
+            T tmp[n * m];
+            matrix_transpose_helper(a, m, n, tmp, m, n);
+            std::copy(tmp, tmp + n * m, b);
+        }
+        else{
+            matrix_transpose_helper(a, m, n, b, m, n);
+        }
     }
 
     template <class T>
